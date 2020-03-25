f0e3ae7e1b5eb232d9f07127b9fcedef982666d9
codymcco <codymccomber@gmail.com>
2020-03-25 13:55:09 -0700

    Updated example

--- examples/main_example.py
+++ examples/main_example.py
@@ -1,11 +1,10 @@
-import simple_schedule
+from simple_schedule import Schedule
 
 s = Schedule()
 
-counter = 0
+
 while True:
-    # print(counter)
-    counter += 1
+
     if s.run_action('5_s'):
         print('5 seconds ran!')
     if s.run_action('10_s'):
